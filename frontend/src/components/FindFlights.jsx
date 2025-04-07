616db430588e752c2a8b0fa7cb5dd7f82caa1bb5
TechGirlMani <ImaniDigitalDiary@gmail.com>
2025-04-07 01:24:10 -0400

    added second set of links

--- frontend/src/components/FindFlights.jsx
+++ frontend/src/components/FindFlights.jsx
@@ -10,12 +10,25 @@ function FindFlights() {
   return (
     <div className='findFlights'> 
       <div className='findFlightsForm'>
+
+        {/* LINKS 1 */}
         <div className='links1'>
+          <ul className='links1'>
+            <li className='links1 book'>Book</li>
+            <li className='links1 flightStatus'>Flight Status</li>
+            <li className='links1 checkIn'>Check-in</li>
+            <li className='links1 myTrips'>My Trips</li>
+          </ul>
+        </div>
+
+        {/* LINKS 2 */}
+        <div className='links2'>
           <ul>
-            <li className='book'>Book</li>
-            <li className='flightStatus'>Flight Status</li>
-            <li className='checkIn'>Check-in</li>
-            <li className='myTrips'>My Trips</li>
+            <li className='links2 flight'>Flight</li>
+            <li className='links2 packages'>Packages</li>
+            <li className='links2 hotel'>Hotel</li>
+            <li className='links2 car'>Car</li>
+            <li className='links2 cruise'>Cruise</li>
           </ul>
         </div>
       </div>
