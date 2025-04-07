7f4f632096b14fac2484c051748ad935bbb09cf0
TechGirlMani <ImaniDigitalDiary@gmail.com>
2025-04-07 01:17:15 -0400

    added background color and flex to FindFlights links1

--- frontend/src/components/FindFlights.jsx
+++ frontend/src/components/FindFlights.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 
-// bootstrap
-import { Form, Container, Navbar, Nav } from 'react-bootstrap'
+// styles
+  import '../styling/navbar.css'
+  // bootstrap
+  import { Form, Container, Navbar, Nav } from 'react-bootstrap'
 
 
 function FindFlights() {
   return (
     <div className='findFlights'> 
-      <Form>
-        <Form.Group className='mb-3'>
-          <Navbar  bg='dark'  data-bs-theme='dark' className="bg-body-tertiary">
-      <Container>
-        <Nav className="me-auto">
-            <Nav.Link href="#book">Book</Nav.Link>
-            <Nav.Link href="#flight-status">Flight Status</Nav.Link>
-            <Nav.Link href="#check-in">Check-in</Nav.Link>
-            <Nav.Link href="#my-trips">My Trips</Nav.Link>
-          </Nav>
-      </Container>
-    </Navbar>
-        </Form.Group>
-      </Form>
+      <div className='findFlightsForm'>
+        <div className='links1'>
+          <ul>
+            <li className='book'>Book</li>
+            <li className='flightStatus'>Flight Status</li>
+            <li className='checkIn'>Check-in</li>
+            <li className='myTrips'>My Trips</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
