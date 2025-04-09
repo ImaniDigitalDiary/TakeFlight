15e193d47a5448c1297c54ea99a62b155f6024bb
TechGirlMani <ImaniDigitalDiary@gmail.com>
2025-04-09 19:14:18 -0400

    added calendar icon to dates input

--- frontend/src/components/DateCalender.jsx
+++ frontend/src/components/DateCalender.jsx
@@ -4,6 +4,8 @@ import React, { useState, useRef }  from 'react'
 import { DateRange } from 'react-date-range'
 import { format } from 'date-fns'
 import { enUS } from 'date-fns/locale'
+import { FaCalendarAlt, FaTimes } from 'react-icons/fa'
+
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
 
@@ -18,33 +20,46 @@ function DateCalender() {
     ])
 
     const [showCalendar, setShowCalendar] = useState(false)
-    const ref = useRef()
+    // const ref = useRef()
 
-    React.useEffect(() => {
-        const handleClickOutside = (event) => {
-        if (ref.current && !ref.current.contains(event.target)) {
-            setShowCalendar(false)
-        }
-        }
-        document.addEventListener('mousedown', handleClickOutside)
-        return () => {
-        document.removeEventListener('mousedown', handleClickOutside)
-        }
-    }, [])
+    // React.useEffect(() => {
+    //     const handleClickOutside = (event) => {
+    //     if (ref.current && !ref.current.contains(event.target)) {
+    //         setShowCalendar(false)
+    //     }
+    //     }
+    //     document.addEventListener('mousedown', handleClickOutside)
+    //     return () => {
+    //     document.removeEventListener('mousedown', handleClickOutside)
+    //     }
+    // }, [])
     return (
-        <div className='relative inline-block'>
-            {/* DATE INPUT BOX */}
+        <div className='relative inline-block text-sm'>
+            {/* DATE INPUT BOX W/ CALENDAR ICON*/}
             <div
-                onClick={() => setShowCalendar(!showCalendar)}
-                className='cursor-pointer border px-4 py-2 rounded-md shadow-sm text-sm bg-white text-gray-700 w-60'
+                onClick={() => setShowCalendar(true)}
+                className='flex items-center justify-between gap-2 cursor-pointer border border-gray-300 rounded-md px-4 py-2 shadow-sm bg-white text-gray-700 w-64'
             >
-                {`${format(range[0].startDate, 'MMM dd')} - ${format(range[0].endDate, 'MMM dd')}`}
-                
+                <span className='datePlaceholder'>
+                    {`${format(range[0].startDate, 'MMM dd')} - ${format(range[0].endDate, 'MMM dd')}`}
+                </span>
+                <FaCalendarAlt className='calendarIcon text-blue-600'  />   
             </div>
 
-            {/* CALENDAR DROPDOWN FOR DATE INPUT*/}
+            {/* CALENDAR MODAL*/}
             {showCalendar && (
-                <div ref={ref} className='absolute mt-2 z-50 shadow-lg'>
+                <div className='absolute mt-2 z-50 shadow-lg bg-white border border-gray-300 p-4 rounded-md'>
+                    {/* CLOSE BUTTON */}
+                    <div className='flex justify-end mb-2'>
+                        <button
+                            onClick={() => setShowCalendar(false)}
+                            className='text-gray-500 hover:text-red-600 text-lg'
+                        >
+                            <FaTimes />
+                        </button>
+                    </div>
+
+
                     <DateRange
                     editableDateInputs={true}
                     onChange={(item) => setRange([item.selection])}
