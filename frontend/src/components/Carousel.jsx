d8588e67d8821242dfeea5f5c6f75b963d140f5f
TechGirlMani <ImaniDigitalDiary@gmail.com>
2025-04-20 20:30:32 -0400

    added carousel to findflights with styling

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import image1 from '../carousel-images/image1.jpg'
+import image2 from '../carousel-images/image2.jpg'
+import image3 from '../carousel-images/image3.jpg'
+import image4 from '../carousel-images/image4.jpg'
+import image5 from '../carousel-images/image5.jpg'
+
+// STYLING
+import '../styling/carousel.css'
+function Carousel() {
+    const images = [image1, image2, image3, image4, image5]
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    // TIMER FOR HOW LONG EACH IMAGE SHOWS BEFORE CHANGING
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+        }, 4000) //4sec interval
+
+        return () => clearInterval(timer)
+    }, [])
+    
+      return (
+    <div>
+        <div 
+            className='carouselBackground'
+            style={{ 
+                backgroundImage: `url(${images[currentIndex]})`,
+                height: '100vh',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                transition: 'background-image 1s ease=in-out'
+            }}
+        />
+        
+    </div>
+  )
+}
+
+export default Carousel
--- frontend/src/components/FindFlights.jsx
+++ frontend/src/components/FindFlights.jsx
@@ -7,115 +7,121 @@ import { BsArrowLeftRight } from "react-icons/bs";
 // components
 import DateCalender from './DateCalender'
 import Travelers from './Travelers'
+import Carousel from './Carousel';
 
-function FindFlights() {
+function FindFlights(images) {
 
   
   return (
-    <div className='findFlights'> 
-      <div className='findFlightsForm'>
+    <div className='findFlightsWrapper'>
+      <Carousel />
 
-        {/* LINKS 1 */}
-        <div className='links1'>
-          <ul className='links1'>
-            <li className='links1 book'>Book</li>
-            <li className='links1 flightStatus'>Flight Status</li>
-            <li className='links1 checkIn'>Check-in</li>
-            <li className='links1 myTrips'>My Trips</li>
-          </ul>
-        </div>
+      {/*  OVERLAY CONTENT */}
+      <div className='findFlightsContent'>
+        <div className='findFlights'> 
+          <div className='findFlightsForm'>
 
-        {/* LINKS 2 */}
-        <div className='links2'>
-          <ul className='links2'>
-            <li className='links2 flight'>Flight</li>
-            <li className='links2 packages'>Packages</li>
-            <li className='links2 hotel'>Hotel</li>
-            <li className='links2 car'>Car</li>
-            <li className='links2 cruise'><span className='cruise'>Cruise</span></li>
-          </ul>
-          <hr />
-        </div>
-
-        {/* RADIO LINKS */}
-        <div className='radioLinks'>
-          <label className='customCircleRadio'>
-            <input type='radio' name='roundTrip' value=''/><span className='circleRadio'></span>Round Trip
-          </label>
-          <label className='customCircleRadio'>
-            <input type='radio' name='oneWay' value=''/><span className='circleRadio'></span>One-way
-          </label>
-          
-          <label className='customSquareRadio'>
-            <input type='radio' name='miles' value='' /> <span className='squareRadio'></span>Book with miles
-          </label>
-          <label className='customSquareRadio'>
-            <input type='radio' name='flexibleDates' value=''/><span className='squareRadio'></span>Flexible dates
-          </label> 
-        </div>
-
-      {/* TEXT INPUTS */}
-        {/* FROM AND TO */}
-        <div className='inputCont'>
-            <div className='fromAndTo'>
-            <div className='fromDiv'>
-              <label className='fromHeading'>
-                From*
-              </label>
-              <input  type='text' id='from' name='from' required className='from' />
+            {/* LINKS 1 */}
+            <div className='links1'>
+              <ul className='links1'>
+                <li className='links1 book'>Book</li>
+                <li className='links1 flightStatus'>Flight Status</li>
+                <li className='links1 checkIn'>Check-in</li>
+                <li className='links1 myTrips'>My Trips</li>
+              </ul>
             </div>
 
-            <div className='toDiv'>
-              <label className='toHeading'>
-                To*
-              </label>
-              <input  type='text' id='to' name='to' required className='to' />
+            {/* LINKS 2 */}
+            <div className='links2'>
+              <ul className='links2'>
+                <li className='links2 flight'>Flight</li>
+                <li className='links2 packages'>Packages</li>
+                <li className='links2 hotel'>Hotel</li>
+                <li className='links2 car'>Car</li>
+                <li className='links2 cruise'><span className='cruise'>Cruise</span></li>
+              </ul>
+              <hr />
             </div>
-          </div>
 
-          {/* DATES AND TRAVELERS */}
-          <div className='datesAndTrav'>
-          <DateCalender />
-          <Travelers />
-          </div>
-        </div>
+            {/* RADIO LINKS */}
+            <div className='radioLinks'>
+              <label className='customCircleRadio'>
+                <input type='radio' name='roundTrip' value=''/><span className='circleRadio'></span>Round Trip
+              </label>
+              <label className='customCircleRadio'>
+                <input type='radio' name='oneWay' value=''/><span className='circleRadio'></span>One-way
+              </label>
+              
+              <label className='customSquareRadio'>
+                <input type='radio' name='miles' value='' /> <span className='squareRadio'></span>Book with miles
+              </label>
+              <label className='customSquareRadio'>
+                <input type='radio' name='flexibleDates' value=''/><span className='squareRadio'></span>Flexible dates
+              </label> 
+            </div>
 
-        {/* FIGHT CLASS */}
-        <div className="chooseClassDiv">
-          <label htmlFor='chooseClassSelect' className='chooseClassHeading'></label>
-          <select name="chooseClass" id="chooseClass" className='chooseClass'>
-            <option value="">Economy</option>
-            <option value="">Premium economy</option>
-            <option value="">Business of First</option>
-          </select>
-        </div>
+          {/* TEXT INPUTS */}
+            {/* FROM AND TO */}
+            <div className='inputCont'>
+                <div className='fromAndTo'>
+                <div className='fromDiv'>
+                  <label className='fromHeading'>
+                    From*
+                  </label>
+                  <input  type='text' id='from' name='from' required className='from' />
+                </div>
 
-        <div className='advSearchDiv'>
-          <div className='advSearchText'>
-            <h3>Advanced search</h3>
-            <p>(Certificates, multi-city and upgrades)</p>
-            <p>Changed bag rules and fees for optional servies</p>
-          </div>
-          <div className='advSearchButtons'>
-            <button className='findFlightsBtn'>Find flights</button>
-            <button className='creditsBtn'>Find your travel credits</button>
-          </div>
-        </div>
+                <div className='toDiv'>
+                  <label className='toHeading'>
+                    To*
+                  </label>
+                  <input  type='text' id='to' name='to' required className='to' />
+                </div>
+              </div>
 
-        
+              {/* DATES AND TRAVELERS */}
+              <div className='datesAndTrav'>
+              <DateCalender />
+              <Travelers />
+              </div>
+            </div>
+
+            {/* FIGHT CLASS */}
+            <div className="chooseClassDiv">
+              <label htmlFor='chooseClassSelect' className='chooseClassHeading'></label>
+              <select name="chooseClass" id="chooseClass" className='chooseClass'>
+                <option value="">Economy</option>
+                <option value="">Premium economy</option>
+                <option value="">Business of First</option>
+              </select>
+            </div>
 
 
-        
-        
+            <div className='advSearchDiv'>
+              <div className='advSearchText'>
+                <h3>Advanced search</h3>
+                <p>(Certificates, multi-city and upgrades)</p>
+                <p>Changed bag rules and fees for optional servies</p>
+              </div>
+              <div className='advSearchButtons'>
+                <button className='findFlightsBtn'>Find flights</button>
+                <button className='creditsBtn'>Find your travel credits</button>
+              </div>
+            </div>
 
-
-        
-        
-        
             
-
+          </div>
+        </div>
       </div>
+
+
+
+      
     </div>
+
+
+
+    
   )
 }
 
