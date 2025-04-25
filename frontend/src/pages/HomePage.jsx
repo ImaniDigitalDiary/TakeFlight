027529a5618b33b3a1b431022734dde1a8ac6cbc
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-04-24 22:23:04 -0400

    imported Navbar and FindFlights components into HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+// COMPONENTS
+import Navbar from '../components/Navbar'
+import FindFlights from '../components/FindFlights'
+
 function HomePage() {
   return (
-    <div>
-      HomePage
+    <div className='homePage'>
+        <Navbar />
+        <FindFlights />
     </div>
   )
 }
