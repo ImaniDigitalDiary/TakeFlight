ce66059bac0e70ddf5a6e40f263ee29b35ff1c40
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-04-24 22:21:17 -0400

    created HomePage and routed link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 
 //  styles
 import './styling/app.css'
 
+// pages
+import HomePage from './pages/HomePage';
 
 // components
 import Navbar from './components/Navbar';
@@ -12,8 +15,11 @@ import FindFlights from './components/FindFlights';
 function App() {
   return (
     <div className='app'>
-      <Navbar />
-      <FindFlights />
+      {/* <Navbar />
+      <FindFlights /> */}
+      <Routes>
+        <Route path='/home' element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
