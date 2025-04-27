ad62c60bf2e0fc64f4d2799b542333eef9daf99e
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-04-26 22:56:36 -0400

    imported Map comp to Homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import FindFlights from '../components/FindFlights'
 import SponsorTravelInspo from '../components/SponsorTravelInspo'
+import Map from '../components/Map'
 
 function HomePage() {
   return (
@@ -11,6 +12,7 @@ function HomePage() {
         <Navbar />
         <FindFlights />
         <SponsorTravelInspo />
+        <Map />
     </div>
   )
 }
