8f8480891cdddd08894367f1b6e215b8d429b731
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-05-10 08:52:42 -0400

    fixed CarouselTwo comp and updated its css

--- frontend/src/components/homepage-components/CarouselTwo.jsx
+++ frontend/src/components/homepage-components/CarouselTwo.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 
 // STYLING
 import 'bootstrap/dist/css/bootstrap.min.css'
+    // FONT AWESOME ICONS
+    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+    import { faCircleRight } from '@fortawesome/free-solid-svg-icons'
+    import { faCircleLeft } from '@fortawesome/free-solid-svg-icons'
 
 // COMPONENTS
 import './homepage-styling/carousel-two.css'
@@ -11,63 +15,85 @@ import './homepage-styling/carousel-two.css'
 import image6 from './carousel-images/image6.jpg'
 import image7 from './carousel-images/image7.jpg'
 import image8 from './carousel-images/image8.jpg'
+import { text } from '@fortawesome/fontawesome-svg-core'
 
 function CarouselTwo() {
     const [index, setIndex] = useState(0)
 
     // handleSelect - update the index to the slide number that the use clicks to
-    const handleSelect = (clickedIndex) => {
-        setIndex(clickedIndex)
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex)
     }
 
-    // move backward one slide
-    const handlePrev = () => {
-        setIndex((prevIndex) => (prevIndex === 0 ? 2 : prevIndex - 1))
+    // click to previous index
+    const goToPrev = () => {
+        setIndex((prev) => (prev === 0 ? 2 : prev - 1))
     }
+
+    // click to next index
+    const goToNext = () => {
+        setIndex((prev) => (prev === 2 ? 0 : prev + 1))
+    }
+
+    const slides = [
+        {
+            image: image6,
+            title: 'Tavel inspiration with you in mind',
+            text: 'Enjoy curated itineraries and hdiden gems from Hemispheres.',
+            button: 'Get inspired',
+            btnClass: 'image6Btn',
+        },
+        {
+            image: image7,
+            title: 'Travel assistance that fits in your hand',
+            text: 'Do the smart thing with your smart phone, Use our app to check-in for your flight, view yourboarding pass, track bags, check your flight status and binge TV shows and movies onboard.',
+            button: 'Download the app',
+            btnClass: 'image7Btn',
+        },
+        {
+            image: image8,
+            title: 'Explore modern-day Rome',
+            text: 'The Colosseum, the Pantheon, the Forum Romanum - visiting ancient Rome is fun. Pasta making, wine tasting, big bus tours - visiting modern Rome is better.',
+            button: 'Book now',
+            btnClass: 'image8Btn',
+        },
+    ]
+
   return (
-    <div>
-      <Carousel indicators={false} controls={true} interval={null} prevIcon={true} nextIcon={true}>
-        <Carousel.Item>
-            <div className='image6TextDiv'>
-                <div className='carouselText'>
-                    <h1>Travel inspiration with you in mind</h1> 
-                    <p>Enjoy curated itineraries and hidden gems from Hemispheres.</p> 
-                    <button className='image6Btn'>Get inspired</button>
-                </div>
-                 <div className='image6Div'>
-                    <img className='image6' src={image6} alt="Travel" />
-                </div>
-            </div> 
-        </Carousel.Item>
-        <Carousel.Item>
-            <div className='image7TextDiv'>
-                <div className='carouselText'>
-                    <h1>Travel assistance that fits in your hand</h1> 
-                    <p>Do the smart thing with your smart phone. Use our app to check-in for your
-                        flight, view your boarding pass, track bags, check your flight status and binge TV shows and movies onboard.
-                    </p> 
-                    <button className='image7Btn'>Download the app</button>
-                </div>
-                 <div className='image7Div'>
-                    <img className='image7' src={image7} alt="Talking on phone" />
-                </div>
-            </div> 
-        </Carousel.Item>
-        <Carousel.Item>
-            <div className='image8TextDiv'>
-                <div className='carouselText'>
-                    <h1>Explore modern-day Rome</h1> 
-                    <p>The Colosseum, the Pantheon, the Forum Romanum - visiting ancient Rome is fun.
-                        Pasta making, wine tasting, big bus tours - visiting modern Rome is better.
-                    </p> 
-                    <button className='image8Btn'>Book now</button>
-                </div>
-                 <div className='image8Div'>
-                    <img className='image8' src={image8} alt="Travel" />
-                </div>
-            </div> 
-        </Carousel.Item>
+    <div className='carouselWrapper'>
+      <Carousel 
+        activeIndex={index}
+        onSelect={handleSelect}
+        indicators={false} 
+        controls={false} 
+        interval={null} 
+        >
+            {slides.map((slide, i) => (
+                
+            <Carousel.Item key={i} className='caroItem'>
+                <div className='image6TextDiv'>
+                    <div className='carouselText'>
+                            <h1>{slide.title}</h1> 
+                            <p>{slide.text}</p> 
+                            <button className={slide.btnClass}>{slide.button}</button>
+                        </div>
+                        <div className='carouselImage'>
+                            <img src={slide.image} alt={`Slide ${i + 1}`} />
+                        </div>
+                        </div>
+            </Carousel.Item>
+            ))}       
       </Carousel>
+
+      {/* prev next buttons */}
+      <div className='customControls'>
+        <button onClick={goToPrev}>
+            <FontAwesomeIcon icon={faCircleLeft} />
+        </button>
+        <button onClick={goToNext}>
+            <FontAwesomeIcon icon={faCircleRight} />
+        </button>
+      </div>
     </div>
   )
 }
