0091714f3bdb63bdfd781a5121316cf9297ad34a
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-05-11 11:42:23 -0400

    created PopularTopics comp

---
+++ frontend/src/components/homepage-components/PopularTopics.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function PopularTopics() {
+  return (
+    <>
+    <div>
+      <h1>Popular topics</h1>
+    </div>
+    <ul>
+        <li>Wi-Fi</li>
+        <li>Change flight</li>
+        <li>United Club</li>
+        <li>Traveling with pets</li>
+        <li>MileagePlus</li>
+        <li>Baggage</li>
+        <li>Refund</li>
+        <li>Help Center</li>
+    </ul>
+    </>
+  )
+}
+
+export default PopularTopics
