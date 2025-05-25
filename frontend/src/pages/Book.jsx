db10584d1f9e450142605cfad70e0d3b95956333
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-05-25 19:22:02 -0400

    imported Navbar comp into BookPage

--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
 
+// COMPONENTS
+import Navbar from '../components/Navbar'
+
 // STYLING
 import '../styling/book-page.css'
 
+
 function Book() {
   return (
-    <>
-    <div className='bookFlightPageMainDiv'>
-        <div className="bookFlightPageMainDiv1">
-            <h1>Book a flight</h1>
-            <div>
-                <a href="signIn">Sign in</a> to your MileagePlus account for even faster checkout.
+    <div className='bookPage'>
+        <Navbar />
+        <div className='bookFlightPageMainDiv'>
+            <div className="bookFlightPageMainDiv1">
+                <h1>Book a flight</h1>
+                <div>
+                    <a href="signIn">Sign in</a> to your MileagePlus account for even faster checkout.
+                </div>
             </div>
+            <div className="bookFlightPageMainDiv2">
+                <h6>Learn more:</h6>
+                <ul>
+                    <li>Unaccompanied minors</li>
+                    <li>Traveling with children</li>
+                    <li>Learn about fare classes</li>
+                    <li>Request a TakeFlight Groups quore for 10+</li>
+                    <li>Pets in cabin</li>
+                    <li>Upgrades overview</li>
+                    <li>Redeeming a future flight credit</li>
+                    <li>Redeeming a certificate</li>
+                    <li>Earning miles</li>
+                </ul>
+            </div>
+        
         </div>
-        <div className="bookFlightPageMainDiv2">
-            <h6>Learn more:</h6>
-            <ul>
-                <li>Unaccompanied minors</li>
-                <li>Traveling with children</li>
-                <li>Learn about fare classes</li>
-                <li>Request a TakeFlight Groups quore for 10+</li>
-                <li>Pets in cabin</li>
-                <li>Upgrades overview</li>
-                <li>Redeeming a future flight credit</li>
-                <li>Redeeming a certificate</li>
-                <li>Earning miles</li>
-            </ul>
-        </div>
-      
-    </div>
 
-    </>
+    </div>
     
   )
 }
