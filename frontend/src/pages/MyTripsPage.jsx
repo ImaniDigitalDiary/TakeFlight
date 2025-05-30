3e4ab81c460a54432b84ef42cb39bb03a6beb3c9
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-05-30 13:25:10 -0400

    created a styling file for mytips page and added display flex

--- frontend/src/pages/MyTripsPage.jsx
+++ frontend/src/pages/MyTripsPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+// STYLING
+import '../styling/my-trips.css'
+
 function MyTripsPage() {
   return (
-    <div>
-      My Trips Page
+    <div className='myTripsPage'>
+        <div className="myTripsDiv1">
+            <div className="myTripsDiv1Text">
+                <h1>My trips</h1>
+                <div>Looking for a receipt? <a href="#">Search receipts</a></div>
+
+            </div>
+            
+
+        </div>
+      
     </div>
   )
 }
