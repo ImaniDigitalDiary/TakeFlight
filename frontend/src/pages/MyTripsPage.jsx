6ba089fbd258cd43752b587d305b9bdddb0daf13
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-05-30 15:13:49 -0400

    added elemenets and styling to myTripsDiv3

--- frontend/src/pages/MyTripsPage.jsx
+++ frontend/src/pages/MyTripsPage.jsx
@@ -14,29 +14,52 @@ function MyTripsPage() {
     <div className='myTripsPage'>
         <Navbar />
         <div className="myTripsPageMainDiv">
+
+            {/* MY TRIPS DIV 1 */}
             <div className="myTripsDiv1">
-            <div className="myTripsDiv1Text">
-                <h1>My trips</h1>
-                <div>Looking for a receipt? <a href="#">Search receipts</a></div>
+                <div className="myTripsDiv1Text">
+                    <h1>My trips</h1>
+                    <div>Looking for a receipt? <a href="#">Search receipts</a></div>
+                </div>
             </div>
-        </div>
 
-
-        <div className='myTripsDiv2'>
+            {/* MY TRIPS DIV 2 */}
+            <div className='myTripsDiv2'>
                 <div className="mileagePlusMainDiv">
-                  <div className='mileagePlusTextAndIcon'> 
+                    <div className='mileagePlusTextAndIcon'> 
                     <FontAwesomeIcon icon={faBookmark} className='fa-xl' /> 
                     <div className='mileagePlusText'>
-                      <h4>Sign in to MileagePlus</h4>
-                      <p>We can use your account information to find your trips.</p> 
+                        <h4>Sign in to MileagePlus</h4>
+                        <p>We can use your account information to find your trips.</p> 
                     </div>              
-                  </div>
-                  <div className='mileagePlusBtns'>
-                      <button className='signInBtn'>Sign in</button>
-                      <button className='joinNowBtn'>Join now</button>
-                  </div>
+                    </div>
+                    <div className='mileagePlusBtns'>
+                        <button className='signInBtn'>Sign in</button>
+                        <button className='joinNowBtn'>Join now</button>
+                    </div>
                 </div>     
-              </div>
+            </div>
+
+            {/* MY TRIPS DIV 3 */}
+            <div className="myTripsDiv3">
+                <div className="myTripsDiv3Heading">
+                    Find a trip
+                </div>
+                <div className="myTripsDiv3InputDiv">
+                    <label htmlFor="">
+                        Confirmation number
+                        <input type='text' />
+                    </label>
+                    <label htmlFor="">
+                        Last name
+                        <input type='text' />
+                    </label>
+                    <div>To add this trip to your account, <span>Sign in</span>.</div>
+                    <button className='nextBtn'>Next</button>
+                </div>
+            </div>
+
+              
 
 
 
