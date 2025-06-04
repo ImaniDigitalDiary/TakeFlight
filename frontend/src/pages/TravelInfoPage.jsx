d679e6d684334b205d98d23d7fee6dfcbd5d1ac7
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-06-03 21:36:02 -0400

    updated styling for .travelInfoImage

--- frontend/src/pages/TravelInfoPage.jsx
+++ frontend/src/pages/TravelInfoPage.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 // COMPONENTS
 import Navbar from '../components/Navbar'
 
+// STYLING
+import '../styling/travel-info.css'
+
+// IMAGES
+import travelInfoImage from '../images/travel-info-image.png'
+
 function TravelInfoPage() {
   return (
     <>
     <Navbar />
-    <div>
-      TravelInfo Page
+    <div className='travelInfoImageDiv'>
+      <img className='travelInfoImage' src={travelInfoImage} alt="Travel Info Image of airplane in the sky" />
     </div>
     
     </>
