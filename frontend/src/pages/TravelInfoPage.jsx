48dddc26df0a0b4631565f6d369ca6a312b43403
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-06-03 21:38:18 -0400

    added TravelInfoPage .gruidelines div and its elements

--- frontend/src/pages/TravelInfoPage.jsx
+++ frontend/src/pages/TravelInfoPage.jsx
@@ -16,6 +16,11 @@ function TravelInfoPage() {
     <div className='travelInfoImageDiv'>
       <img className='travelInfoImage' src={travelInfoImage} alt="Travel Info Image of airplane in the sky" />
     </div>
+
+    <div className="guidelines">
+        <h1 className="guidelinesHeader">Travel information and guidelines</h1>
+        <p className="guidelinesHeaderPara">Use the information on this page to help you through your next trip.</p>
+    </div>
     
     </>
   )
