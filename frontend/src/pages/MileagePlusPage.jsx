0876112815a597e250f2d82e36c4d31f0ecf501d
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-06-07 18:00:32 -0400

    added heading and headPara to .milePlusHeadText

--- frontend/src/pages/MileagePlusPage.jsx
+++ frontend/src/pages/MileagePlusPage.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function MileagePlusPage() {
   return (
     <div>
-      Mileage Plus
+      <div className="milePlusMain">
+        <div className="milePlusHeadText">
+            <h1>Create account</h1>
+            <p>Start your MileagePlus adventure today. Our loyalty program is free to join and the miles you earn 
+                will never expire. We reward you travel and regular activities, so you can earn miles every day of the year.
+            </p>
+        </div>
+      </div>
     </div>
   )
 }
