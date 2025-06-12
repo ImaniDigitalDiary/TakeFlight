3b50f24e392f305a06b614b6a5acdc64b461ad33
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-06-12 15:54:41 -0400

    imported navbar comp into dealspage

--- frontend/src/pages/DealsPage.jsx
+++ frontend/src/pages/DealsPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+// COMPONENTS
+import Navbar from '../components/Navbar'
+
 function DealsPage() {
   return (
     <div>
-      <h1>Travel deals</h1>
+        <Navbar />
+        <div className="dealsMain">
+            <h1>Travel deals</h1>
+
+        </div>
+      
     </div>
   )
 }
