70b4c4a0d5f71530af170b1ae4e6db851b1c6388
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-06-12 15:59:30 -0400

    added rest of elements into .dealsOfferLinksDiv

--- frontend/src/pages/DealsPage.jsx
+++ frontend/src/pages/DealsPage.jsx
@@ -12,13 +12,17 @@ function DealsPage() {
             <p>Enjoy discounts on over 4,500 flights to 300+ destinations.
                 Some Take Flight deals also let you earn extra miles.
             </p>
-            <div className="dealsOfferLinks">
+            <div className="dealsOfferLinksDiv">
+                <div className="dealsOfferLinks">
                 <div className="specialOffers">
                     <a href="#">Special flight offers</a>
                 </div>
                 <div className="otherOffers">
                     <a href="#">Other offers</a>
                 </div>
+            </div>
+            <h3>Special flight offers</h3>
+            <p>You can explore our current flight offers and promotions using our <span><a href="#">search tool</a></span>.</p>
             </div>
         </div>
       
