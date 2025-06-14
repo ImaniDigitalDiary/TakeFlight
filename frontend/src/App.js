f7936bcd5249551c8fef87f31623033b3513d23c
ImaniDigitalDiary <ImaniDigitalDiary@gmail.com>
2025-06-14 13:01:14 -0400

    added css for mediascreen size 2560px for DealsPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {Routes, Route} from 'react-router-dom'
 
 //  STYLES
 import './styling/app.css'
+  // QUERIES
+  import '../src/styling/media-queries/screen-2560.css'
 
 // PAGES
 import HomePage from './pages/HomePage';
